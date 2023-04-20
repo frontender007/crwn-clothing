8c21acff21ce6645eab2981ae57950f767e9dab8
Mr Akhtar <mogulking@gmail.com>
2023-04-20 17:06:55 +0100

    added routing and navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import "./categories.styles.scss";
-import Categories from "./components/categories/Categories.component";
+import React from "react";
+import Home from "./routes/home/Home.component";
+import Navigation from "./routes/navigation/Navigation.component";
+import { Routes, Route } from "react-router-dom";
+
+const Shop = () => {
+  return <h1>Shop page is rendered.</h1>;
+};
 
 const App = () => {
- 
   return (
-    <Categories />
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+      </Route>
+    </Routes>
   );
 };
 
