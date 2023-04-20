4dbd7d1db5a88aac54b49beb0cc7f5226a4eae21
Mr Akhtar <mogulking@gmail.com>
2023-04-20 12:36:53 +0100

    Captsone project setup part 1

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./categories.styles.scss";
+import Categories from "./components/categories/Categories.component";
+
+const App = () => {
+ 
+  return (
+    <Categories />
+  );
+};
+
+export default App;
---
+++ src/components/category-item/category-item.component.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./category-item.styles.scss";
+
+const CategoryItem = ({ id, title, imageUrl }) => {
+  return (
+    <div className="category-container">
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
+      <div className="category-body-container">
+        <h2>{title}</h2>
+        <p>Show Now</p>
+      </div>
+    </div>
+  );
+};
+
+export default CategoryItem;
